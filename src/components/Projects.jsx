456a08f72c7b464df4b353b8be10c2fb5b418427
Nicole Sanchez <sancheznncole@gmail.com>
2025-09-30 19:48:16 +0800

    style: add styling to links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,10 +7,15 @@ function Projects({project}) {
         title="Projects" />
       {project.map((proj, key) => (
         <div className="card" key={key.id}>
-          <h2 className="card-title">
-            {proj.title}
-            {proj.isWIP ? <span className="wip"> (wip)</span> : null}
-          </h2>
+          <a href={proj.url} target="_blank">
+            <h2 className="card-title ref-link">
+              {proj.title} 
+                <span>
+                  <svg  className="link-icon" xmlns="http://www.w3.org/2000/svg" width="12px" height="12px" viewBox="0 0 21 21" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+                </span>
+              {proj.isWIP ? <span className="wip"> (wip)</span> : null}
+            </h2>
+          </a>
           <div className="card-description">
             <span className="detail">{proj.detail}</span>
           </div>
